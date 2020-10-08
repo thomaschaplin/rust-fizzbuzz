62bd313caba5cccbafbd84d863ec8c0a7d3c84d3
Thomas Chaplin <thomas.chaplin@tray.io>
2020-10-08 01:13:08 +0100

    Inital commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn fizzbuzz(count: i32) -> () {
+    for i in 0..=count {
+        match (i % 3, i % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            (_, _) => println!("{}", i),
+        }
+    }
+}
+
+fn main() {
+    fizzbuzz(100);
+}
